3c2c9373656c0b0d2302698e85f3c3371db39bde
Pavel Sakharov <p.sakharov@kefirgames.com>
2020-11-22 14:07:34 +0300

    add: project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElectronicVoting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
