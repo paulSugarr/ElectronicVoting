aadea4db4db8eaa512297c071db525abfc477e31
paulSugarr <paul.sugar.a@gmail.com>
2020-12-10 16:13:27 +0300

    fix: merge resolved.

--- Electors/Elector.cs
+++ Electors/Elector.cs
@@ -7,33 +7,32 @@ namespace ElectronicVoting.Electors
 {
     public class Elector
     {
-        private byte[] _privateKey;
-        private byte[] _publicEncryptionKey;
-        private byte[] _publicSignKey;
-        private byte[] _blindKey;
+        private Dictionary<string, object> _privateKey;
+        private Dictionary<string, object> _publicEncryptionKey;
+        private Dictionary<string, object> _publicSignKey;
+        private Dictionary<string, object> _blindKey;
+
+        private ICryptographyProvider _cryptographyProvider;
 
         public Elector()
         {
             
         }
-        public Elector(Dictionary<string, object> initialData)
+        public Elector(ICryptographyProvider cryptographyProvider, Dictionary<string, object> initialData)
         {
-            var privateKeyString = initialData.GetString("private_key");
-            _privateKey = Encoding.UTF8.GetBytes(privateKeyString);
-            var publicEncryptKeyString = initialData.GetString("public_encrypt_key");
-            _publicEncryptionKey = Encoding.UTF8.GetBytes(publicEncryptKeyString);
-            var publicSignKeyString = initialData.GetString("public_sign_key");
-            _publicSignKey = Encoding.UTF8.GetBytes(publicSignKeyString);
-            var blindKeyString = initialData.GetString("blind_key");
-            _blindKey = Encoding.UTF8.GetBytes(blindKeyString);
+            _cryptographyProvider = cryptographyProvider;
+            _privateKey = initialData.GetDictionary("private_key");
+            _publicEncryptionKey = initialData.GetDictionary("public_encrypt_key");
+            _publicSignKey = initialData.GetDictionary("public_sign_key");
+            _blindKey = initialData.GetDictionary("blind_key");
         }
 
         public void CreateNewKeys()
         {
-            _privateKey = KeyCreator.CreatePrivateKey();
-            _publicEncryptionKey = KeyCreator.CreatePublicKey(_privateKey);
-            _publicSignKey = KeyCreator.CreatePublicKey(_privateKey);
-            _blindKey = KeyCreator.CreatePublicKey(_privateKey);
+            _privateKey = RSAKeyCreator.CreatePrivateKey();
+            _publicEncryptionKey = RSAKeyCreator.CreatePublicKey(_privateKey);
+            _publicSignKey = RSAKeyCreator.CreatePublicKey(_privateKey);
+            _blindKey = RSAKeyCreator.CreatePublicKey(_privateKey);
         }
         
         /// <summary> Step 2 in E-voting protocol</summary>
@@ -41,21 +40,21 @@ namespace ElectronicVoting.Electors
         {
             var bulletin = CreateBulletin(0);
             var data = Encoding.UTF8.GetBytes(bulletin);
-            var encryptB = CryptographyProvider.Encrypt(_publicEncryptionKey, data);
-            var signEncryptB = CryptographyProvider.SignData(_publicSignKey, encryptB);
-            var blindSignEncryptB = CryptographyProvider.Encrypt(_blindKey, signEncryptB);
+            var encryptB = _cryptographyProvider.Encrypt(_publicEncryptionKey, data);
+            var signEncryptB = _cryptographyProvider.SignData(_publicSignKey, encryptB);
+            var blindSignEncryptB = _cryptographyProvider.Encrypt(_blindKey, signEncryptB);
             return blindSignEncryptB;
         }
         
         /// <summary> Step 4 in E-voting protocol</summary>
         public byte[] RemoveBlindEncryption(byte[] blindedMessage)
         {
-            var result = CryptographyProvider.Decrypt(_privateKey, blindedMessage);
+            var result = _cryptographyProvider.Decrypt(_privateKey, blindedMessage);
             return result;
         }
         
         /// <summary> Step 6 in E-voting protocol</summary>
-        public byte[] GetPrivateKey()
+        public Dictionary<string, object> GetPrivateKey()
         {
             return _privateKey;
         }
